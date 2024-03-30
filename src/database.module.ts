7f9eeffffd6c71de407fcd891a870061c8065fb6
Uhmpasterig <805luca805@gmail.com>
2024-03-31 00:51:17 +0100

    feat: add dynamic module and client for prisma

--- src/database.module.ts
+++ src/database.module.ts
@@ -1,70 +1,44 @@
-import { DynamicModule, Global, Module } from "@nestjs/common";
-import { ConfigService } from "./database.service";
-import {
-  ClientProviderOptions,
-  ClientsModule,
-  Transport,
-} from "@nestjs/microservices";
-
-// Import dotenv to read the environment variables
-import * as dotenv from "dotenv";
-dotenv.config();
-
-// Read the port from the environment variables
-const CONFIG_SERVICE_PORT = parseInt(process.env.CONFIG_SERVICE_PORT, 10);
-if (isNaN(CONFIG_SERVICE_PORT)) {
-  throw new Error("CONFIG_SERVICE_PORT is not defined in the environment");
-}
-
-const createConfigServiceOptions = (port: number): ClientProviderOptions => ({
-  name: "R_CONFIG_SERVICE",
-  transport: Transport.TCP,
-  options: {
-    port,
-  },
-});
-
-const createConfigServiceClient = (port: number) =>
-  ClientsModule.registerAsync([
-    {
-      name: "R_CONFIG_SERVICE",
-      useFactory: () => createConfigServiceOptions(port),
-    },
-  ]);
+import { Global, Module } from "@nestjs/common";
+import { PrismaService } from "./database.service";
+import { Prisma } from "@prisma/client";
 
 @Global()
 @Module({})
-export class ConfigModule {
-  static forRoot(
-    configServicePort: number = CONFIG_SERVICE_PORT
-  ): DynamicModule {
+export class DatabaseModule {
+  private static createDynamicModule(options: Prisma.PrismaClientOptions) {
     return {
-      module: ConfigModule,
-      exports: [ConfigService],
-      providers: [ConfigService],
-      imports: [createConfigServiceClient(configServicePort)],
+      module: DatabaseModule,
+      providers: [
+        {
+          provide: PrismaService,
+          useValue: new PrismaService(options),
+        },
+      ],
+      exports: [PrismaService],
     };
   }
 
-  static forRootAsync(
-    configServicePort: number = CONFIG_SERVICE_PORT
-  ): DynamicModule {
-    return {
-      module: ConfigModule,
-      exports: [ConfigService],
-      providers: [ConfigService],
-      imports: [createConfigServiceClient(configServicePort)],
-    };
+  static forRoot(options: Prisma.PrismaClientOptions) {
+    return this.createDynamicModule(options);
   }
 
-  static register(
-    configServicePort: number = CONFIG_SERVICE_PORT
-  ): DynamicModule {
-    return {
-      module: ConfigModule,
-      exports: [ConfigService],
-      providers: [ConfigService],
-      imports: [createConfigServiceClient(configServicePort)],
-    };
+  static forRootAsync(options: {
+    useFactory: () => Prisma.PrismaClientOptions;
+  }) {
+    return this.createDynamicModule(options.useFactory());
+  }
+
+  static register(options: Prisma.PrismaClientOptions) {
+    return this.createDynamicModule(options);
+  }
+
+  static registerAsync(options: {
+    useFactory: () => Prisma.PrismaClientOptions;
+  }) {
+    return this.createDynamicModule(options.useFactory());
+  }
+
+  static forFeature() {
+    // This method is a placeholder for future use
   }
 }
--- src/database.service.ts
+++ src/database.service.ts
@@ -1,9 +1,12 @@
-
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(private readonly options: Prisma.PrismaClientOptions) {
+    super(options);
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
