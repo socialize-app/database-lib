4d5eed5b63b1da7b29c586029188ff4c4d9008a1
Uhmpasterig <805luca805@gmail.com>
2024-03-31 01:37:32 +0100

    test: try to export .ts files and using a postinstall

--- src/database.module.ts
+++ src/database.module.ts
@@ -1,6 +1,6 @@
 import { Module, DynamicModule, Global } from "@nestjs/common";
 import { PrismaService } from "./database.service";
-import { Prisma } from "@prisma/client";
+import { Prisma } from "./prisma-client";
 
 @Global()
 @Module({})
