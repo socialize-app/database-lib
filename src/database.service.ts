39cba7502563a15032e965fffd63a3d317104a58
Uhmpasterig <805luca805@gmail.com>
2024-03-31 03:19:44 +0200

    debug: added even more debug prints

--- src/database.service.ts
+++ src/database.service.ts
@@ -5,6 +5,8 @@ import { Prisma, PrismaClient } from "./prisma-client";
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor(private readonly options: Prisma.PrismaClientOptions) {
     console.log("Got options:", options);
+    console.log("DATABASE_URL:", process.env.DATABASE_URL);
+    console.log("ENV:", process.env.NODE_ENV);
     super(options);
   }
 
