22ce09397bd5930adc7fefd6d72db3233292e926
Uhmpasterig <805luca805@gmail.com>
2024-03-31 03:00:46 +0200

    feat: removed postinstall script

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-export function createContext(ctx: any): Promise<Context>;
-
 export * from "./prisma-client/index.d";
 
 export interface Context {
