307d2a7673cb94a00ea8ebc4667cbb2f737d0f30
Uhmpasterig <805luca805@gmail.com>
2024-03-31 03:32:34 +0200

    feat: fix of aggregate error, remove dbg prints

--- src/database.service.ts
+++ src/database.service.ts
@@ -1,21 +1,13 @@
-import { Injectable, OnModuleInit, Logger } from "@nestjs/common";
+import { Injectable, OnModuleInit } from "@nestjs/common";
 import { Prisma, PrismaClient } from "./prisma-client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  private readonly logger = new Logger(PrismaService.name);
-
   constructor(private readonly options: Prisma.PrismaClientOptions) {
     super(options);
   }
 
   async onModuleInit() {
-    try {
-      await this.$connect();
-      this.logger.log("Database connection established successfully.");
-    } catch (error) {
-      this.logger.error("Error connecting to database:", error);
-      throw error; // Rethrow the error to propagate it further if needed
-    }
+    await this.$connect();
   }
 }
